a6ee0f4142e4cffe9fec6c0029002901e8ffccfe
worldgr8tst <66232919+worldgr8tst@users.noreply.github.com>
2022-10-12 13:23:14 +0100

    github-finder

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/Users/UserResults"
+import UserSearch from "../components/Users/UserSearch"
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+       <UserResults /> 
+    </>
+  )
+}
+
+export default Home
